ac1426b65d4589d5e79675a406a87aef89e1d4ac
NIkita Chernykh <chernykhmain@gmail.com>
2023-10-13 09:46:21 +0400

    added legal links

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
     <div className="App">   
       <div className="background"></div>
       <RepeatingImage />
-    
       <div className="content-container">
         <div className="stars-container">
           <Stars />
@@ -20,6 +19,47 @@ function App() {
           <Globe />
         </div>
       </div>
+      <section class="column-section">
+        <div class="column">
+          <img src={require('./Images/Map.png')} alt="map" />
+          <h2>Explore the world map</h2>
+          <p>Explore an entirely new world map designed for geography learning! Each continent serves as a distinct learning section, allowing you to focus on individual regions.</p>
+        </div>
+        <div class="column">
+          <img src={require('./Images/Canada.png')} alt="capital" />
+          <h2>Learn Capitals</h2>
+          <p>Discover a fun and interactive way to learn the capitals of countries from around the world, where you can test your knowledge of global city names.</p>
+        </div>
+    
+        <div class="column">
+          <img src={require('./Images/Geo.png')} alt="geo" />
+          <h2>Become the Geo Genius</h2>
+          <p>Pass all the tests and test your knowledge to claim the Geo Genius status as you embark on a journey to master world geography, identifying capitals and more through exciting challenges and quizzes.</p>
+        </div>
+      </section>
+      <footer className="footer">
+        <div className="divider"></div>
+        <div className="links">
+          {/* <div class="social-icons">
+            <a href="#">i</a>
+            <a href="#">b</a>
+            <a href="#">c</a>
+            <a href="#">d</a>
+          </div> */}
+          {/* <div className="footer-links">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+            <a href="#">Link 4</a>
+            <a href="#">Link 5</a>
+          </div> */}
+        </div>
+        <div className="copyright">
+          &copy; 2023 Chernika Digital - FZCO. All Rights Reserved.
+            <a href="https://www.chernikadigital.com/privacy-policy" target='_blank' className='legal-link'>Privacy Policy</a>
+            <a href="https://www.chernikadigital.com/terms-of-service" target='_blank' className='legal-link'>Terms of Use</a>
+        </div>
+      </footer>
     </div>
   );
 }
--- src/Logo.js
+++ src/Logo.js
@@ -3,7 +3,7 @@ import './Logo.css';
 
 
 function Logo() {
-  return <div className="logo"></div>;
+  return <div className="logo"><h1 className='coming-soon'>Coming Soon!</h1></div>;
 }
 
 export default Logo;
