fa8dc5ed8da861a42468470d3a901a7ac4c02575
NIkita Chernykh <chernykhmain@gmail.com>
2024-02-18 13:17:43 +0400

    Add mailchimp email subscription

--- src/components/newsletter/newsletter.tsx
+++ src/components/newsletter/newsletter.tsx
@@ -1,20 +1,55 @@
-import React from 'react'
+import axios from 'axios'
+import { FormEvent, useState } from 'react'
+import styles from './newsletter.module.scss'
 
 const Newsletter = () => {
+  const [email, setEmail] = useState<string>('')
+  const [status, setStatus] = useState<
+    'success' | 'error' | 'loading' | 'idle'
+  >('idle')
+  const [responseMsg, setResponseMsg] = useState<string>('')
+  const [statusCode, setStatusCode] = useState<number>()
+
+  const handleSubscribe = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setStatus('loading')
+    try {
+      const response = await axios.post('/api/subscribe', { email })
+
+      setStatus('success')
+      setStatusCode(response.status)
+      setEmail('')
+      setResponseMsg(response.data)
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        setStatus('error')
+        setStatusCode(err.response?.status)
+        setResponseMsg(err.response?.data)
+      }
+    }
+  }
+
   return (
-    <form className="rounded px-8 pt-6 pb-8 mb-4 max-w-md">
+    <form onSubmit={handleSubscribe} noValidate>
       <div className="flex">
         <input
-          className="grow mr-1 transition ease-out delay-75 focus-within:border-2 focus-within:border-purple-600 items-center h-14 pr-0.5 border border-purple-600 rounded caret-purple-700 outline-none px-4 disabled:border-slate-400"
           type="email"
-          placeholder="What is your email address?"
+          placeholder="Email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          disabled={status == 'loading'}
         />
-        <button
-          className="bg-violet-700 hover:bg-violet-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-slate-400"
-          type="submit"
-        >
+        <button type="submit" disabled={status == 'loading'}>
           Subscribe
         </button>
+      </div>
+      <div>
+        {status === 'success' ? (
+          <p className={styles.success}>{responseMsg}</p>
+        ) : null}
+        {status === 'error' ? (
+          <p className={styles.error}>{responseMsg}</p>
+        ) : null}
       </div>
     </form>
   )
